cf12b1f9b29c9b5d543f4320e85600b1b6d00ddf
unknown <yakumoft@gmail.com>
2018-09-16 15:27:54 +0300

    fix bug with preload

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,8 +6,21 @@ class BootScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('ship', '/dist/assets/spaceShips_001.png')
-        this.scene.start('TitleScene');
+        const progress = this.add.graphics();
+
+        this.load.image('ship', './dist/assets/spaceShips_001.png');
+
+        this.load.on('progress', value => {
+            progress.clear();
+            progress.fillStyle(0xffffff, 1);
+            progress.fillRect(0, this.sys.game.config.height / 2, this.sys.game.config.width * value, 60);
+        });
+
+        this.load.on('complete', () => {
+            // prepare all animations, defined in a separate file
+            progress.destroy();
+            this.scene.start('TitleScene');
+        });
     }
 
 }
