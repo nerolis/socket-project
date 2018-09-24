9370f69c5e1a485b0fa8ce1b36c3de1dd98523f2
nerolis <yakumoft@gmail.com>
2018-09-24 13:49:04 +0300

    fix eslint bug

--- gameServer.js
+++ gameServer.js
@@ -13,10 +13,10 @@ class Player {
 		return {
 			playerId 	: this.socket.id,
 			rotation	: 0,
-			hp			: 100,
+			hp		: 100,
 			speed		: 2,
-			x			: Math.floor(Math.random() * 700) + 50,
-			y			: Math.floor(Math.random() * 500) + 50,
+			x		: Math.floor(Math.random() * 700) + 50,
+			y		: Math.floor(Math.random() * 500) + 50,
 			team		: (Math.floor(Math.random() * 2) == 0) ? '0x0000ff' : '0xff0000',
 			alive		: true
 		};
